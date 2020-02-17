35654680e77a282abfd39376d258b47e8ecfc9b9
amberlu00 <amberlu00@gmail.com>
2020-02-17 17:01:46 -0600

    loaded files

---
+++ src/TrainerApp.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+// const idStore = createStore();
+
+function TrainerApp() {
+    return(
+        <div className="TrainerMainPage">
+            <p>Loading my trainees...</p>
+        </div>
+    );
+}
+
+export default TrainerApp;
---
+++ src/index.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './TrainerApp';
+
+ReactDOM.render(<App />, document.getElementById('root'));
